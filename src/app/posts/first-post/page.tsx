51094ef0e974f43c3b45ccb57aceb22b762a68fc
victor sato <victor@zeta.inc>
2023-06-01 17:55:28 +0900

    [add]レイアウトコンポーネントを追加

--- src/app/posts/first-post/page.tsx
+++ src/app/posts/first-post/page.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 
+import Layout from '@/components/layout';
+
 export const metadata = {
   title: 'First Post',
 };
 
 export default async function Page() {
   return (
-    <>
+    <Layout>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
