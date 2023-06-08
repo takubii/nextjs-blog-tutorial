7c77503c90134df9e3eff841eea72647e2678924
takubii <vicsat93@gmail.com>
2023-06-09 00:48:39 +0900

    [add]ダイナミックルートのIDを使用して内容を変更する処理を追加

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,19 @@
 import Layout from '@/components/layout';
-import { getAllPostIds } from '@/lib/posts';
+import { getAllPostIds, getPostData } from '@/lib/posts';
 
 export async function generateStaticParams() {
   return getAllPostIds();
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ params }: { params: { id: string } }) {
+  const postData = getPostData(params.id);
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
