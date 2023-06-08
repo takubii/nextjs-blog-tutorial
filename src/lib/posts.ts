997359c78227ea4892a1d77dbf1d51b1de71ad94
victor sato <victor@zeta.inc>
2023-06-08 16:59:27 +0900

    [fix]app routerではparamが必要ないので修正

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -40,9 +40,7 @@ export function getAllPostIds() {
 
   return fileNames.map((fileName) => {
     return {
-      params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
+      id: fileName.replace(/\.md$/, ''),
     };
   });
 }
