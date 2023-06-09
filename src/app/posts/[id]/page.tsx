0be2d6ee2b28cef0c80d3b1317dae0801a20cac6
victor sato <victor@zeta.inc>
2023-06-09 14:57:42 +0900

    [add]動的なメタデータを追加

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -5,6 +5,12 @@ export async function generateStaticParams() {
   return getAllPostIds();
 }
 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const postData = await getPostData(params.id);
+
+  return { title: postData.title };
+}
+
 export default async function Post({ params }: { params: { id: string } }) {
   const postData = await getPostData(params.id);
   return (
