7deb8b195dfc385c57b34fb12938df6d38ad77c8
takubii <vicsat93@gmail.com>
2023-06-09 22:23:10 +0900

    [add]日付表示をDateコンポーネントに変更し、JSXを修正

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Date from '@/components/date';
 import Layout from '@/components/layout';
 import { getAllPostIds, getPostData } from '@/lib/posts';
+import utilStyles from '@/styles/utils.module.css';
 
 export async function generateStaticParams() {
   return getAllPostIds();
@@ -15,13 +17,13 @@ export default async function Post({ params }: { params: { id: string } }) {
   const postData = await getPostData(params.id);
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
