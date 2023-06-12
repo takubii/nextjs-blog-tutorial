5c55eb6f2ab5fda5846c67301313c78f8a33d771
takubii <vicsat93@gmail.com>
2023-06-12 21:46:24 +0900

    [refactor]metadataをlayout.tsxからpage.tsxに移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,27 @@ import Link from 'next/link';
 
 import { getSortedPostsData } from '@/lib/posts';
 import Date from '@/components/date';
-import Layout, { siteTitle, metaData } from '@/components/layout';
+import Layout from '@/components/layout';
 import utilStyles from '@/styles/utils.module.css';
+
+export const siteTitle = 'Next.js Sample Website';
 
 export const metadata = {
   title: siteTitle,
-  ...metaData,
+  description: 'Learn how to build a personal website using Next.js',
+  openGraph: {
+    images: [
+      {
+        url: `https://og-image.vercel.app/${encodeURI(
+          siteTitle
+        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`,
+      },
+    ],
+    title: siteTitle,
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
 
 export default function Home() {
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,24 +5,6 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 
 const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
-
-export const metaData = {
-  description: 'Learn how to build a personal website using Next.js',
-  openGraph: {
-    images: [
-      {
-        url: `https://og-image.vercel.app/${encodeURI(
-          siteTitle
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`,
-      },
-    ],
-    title: siteTitle,
-  },
-  twitter: {
-    card: 'summary_large_image',
-  },
-};
 
 export default function Layout({ children, home }: { children: React.ReactNode; home?: boolean }) {
   return (
